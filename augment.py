c1ed4fbf89f067a6ce9ae4dcbc60ac448b21ac99
Nihar <nihar1912@live.com>
2020-04-19 13:24:59 -0600

    '1'

---
+++ augment.py
@@ -0,0 +1,42 @@
+import os
+import csv
+import numpy as np
+import itertools
+import sys
+os.chdir(sys.path[0])
+
+def augment():
+    car_images=[]
+    steering_angles=[]
+    samples=[]
+    with open(r".\data\driving_log.csv") as csvfile:
+        reader=csv.reader(csvfile)
+        for line in reader:
+            samples.append(line)
+        samples=samples[1:]
+        for line in samples:
+            steering_center=float(line[3])
+
+            correction=0.2
+            steering_left=str(float(steering_center)+correction)
+            steering_right=str(float(steering_center)-correction)
+
+            img_center=line[0]
+            img_left=line[1]
+            img_right=line[2]
+
+
+            # add images and angles to data set
+            car_images.extend([img_center, img_left, img_right])
+            steering_angles.extend([steering_center, steering_left, steering_right])
+
+        samples=[list(a) for a in zip(car_images, steering_angles)]
+        print(len(samples))
+        print(len(samples[0]))
+
+        with open('modified.csv', 'w') as filehandle:
+            for listitem in samples:
+                filehandle.write('%s' % listitem[0])
+                filehandle.write(',' )
+                filehandle.write('%s' % listitem[1])
+                filehandle.write('\n')
---
+++ train.py
@@ -0,0 +1,94 @@
+import os
+import csv
+import sys
+import augment
+
+os.chdir(sys.path[0])
+
+augment.augment()
+
+# load the samples csv
+samples=[]
+with open(r".\modified.csv") as csvfile:
+    reader=csv.reader(csvfile)
+    for line in reader:
+        samples.append(line)
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import ShuffleSplit as shuffle
+# split the samples data set by 80 20 ratio
+train_samples, validation_samples = train_test_split(samples,test_size=0.2)
+
+import cv2
+import numpy as np
+import sklearn
+# create a generator that loads the images
+def generator(samples,batch_size=32):
+    num_samples=len(samples)
+    while 1:
+        # shuffle the num_samples
+        shuffle(samples)
+
+        for offset in range(0,num_samples,batch_size):
+            batch_samples=samples[offset:offset+batch_size]
+            images=[]
+            angles=[]
+            # iterate through each line in batch samples
+            # and append to list
+            for batch_sample in batch_samples:
+                # load the image
+                name='./data/IMG/'+batch_sample[0].split('/')[-1]
+                img=cv2.imread(name)
+                angle=batch_sample[1]
+                images.append(img)
+                angles.append(angle)
+
+
+
+            X_train=np.array(images)
+            y_train=np.array(angles)
+
+            yield sklearn.utils.shuffle(X_train,y_train)
+
+train_generator=generator(train_samples)
+validation_generator=generator(validation_samples)
+
+import tensorflow as tf
+import keras
+from keras.models import Sequential
+from keras.layers import Cropping2D
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers.core import Dense,Activation,Flatten,Lambda
+from keras.layers import Lambda
+from math import ceil
+
+batch_size=32
+model=Sequential()
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((50,20),(0,0))))
+model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
+model.add((Convolution2D(36,5,5,subsample=(2,2),activation="relu")))
+model.add(Convolution2D(63,3,3,activation="relu"))
+model.add(Convolution2D(63,3,3,activation="relu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+model.compile(loss='mse',optimizer='adam',metrics=['accuracy'])
+history=model.fit_generator(train_generator,
+steps_per_epoch=ceil(len(train_samples)/batch_size),
+validation_data=validation_generator,
+validation_steps=ceil(len(validation_samples)/batch_size),
+epochs=5,verbose=1)
+
+print("Completed Training")
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
