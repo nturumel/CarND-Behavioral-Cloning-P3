8867463b8cfe7853c37d06b6135ef0066be10c66
Nihar Turumella <nihar1912@live.com>
2020-11-14 22:35:32 -0600

    same change 3

--- train.py
+++ train.py
@@ -80,4 +80,4 @@ if __name__ == "__main__":
     train_generator, valid_generator = buildGenerator()
     model = buildModel()
     checkpoint = ModelCheckpoint("steering_prediction_model.h5", monitor='val_loss', verbose=1, save_best_only=True, mode='auto', period=1)
-    model.fit_generator(train_generator, steps_per_epoch = train_generator.samples // BATCH_SIZE, validation_data = valid_generator, validation_steps = validvalid_generatoration_generator.samples // BATCH_SIZE, epochs = nb_epochs, callbacks=[checkpoint])
+    model.fit_generator(train_generator, steps_per_epoch = train_generator.samples // BATCH_SIZE, validation_data = valid_generator, validation_steps = valid_generator.samples // BATCH_SIZE, epochs = nb_epochs, callbacks=[checkpoint])
