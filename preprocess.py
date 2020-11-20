cea4272b6ab8e62ff04f5a91bd9d98887bbe1072
Nihar Turumella <nihar1912@live.com>
2020-11-20 08:41:03 -0600

    only center

--- preprocess.py
+++ preprocess.py
@@ -16,16 +16,10 @@ def preprocess():
         reader = csv.reader(csvfile)
         for line in reader:
             steering_center = float(line[3])
-            steering_left = str(float(steering_center) + correction)
-            steering_right = str(float(steering_center) - correction)
-
+     
             center_filename = ntpath.basename(line[0])
-            left_filename = ntpath.basename(line[1])
-            right_filename = ntpath.basename(line[2])
-
+          
             filename_angles.append([center_filename, steering_center])
-            filename_angles.append([left_filename, steering_left])
-            filename_angles.append([right_filename, steering_right])
             
     with open(data_out, 'w') as filehandle:
         for line in filename_angles:
